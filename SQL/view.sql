375b862532d87d90afcaa0f3ce71a71fdb20e8da
alim salehzade <salehzade89@gmail.com>
2018-09-04 21:43:10 +0400

    .

--- SQL/view.sql
+++ SQL/view.sql
@@ -1,10 +1,25 @@
 use db
 
-select * from Lesson
+select *
+from Lesson
 
-INSERT into Lesson VALUES('Lesson5' , 700 , 1);
+INSERT into Lesson
+VALUES('Lesson5' , 700 , 1);
 
 CREATE VIEW dbo.LessonPrice
-as SELECT * from dbo.Lesson WHERE default_price > 300
+as
+    SELECT *
+    from dbo.Lesson
+    WHERE default_price > 300
 
-select * FROM LessonPrice
+select *
+FROM LessonPrice
+
+SELECT *
+FROM Groupc
+
+CREATE VIEW GroupDayCountPay
+as
+    SELECT grou_day_count_pay
+    FROM Groupc
+    WHERE grou_day_count_pay>10
