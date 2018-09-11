388d97a8e234a6174d430745ffb569acd8cd7c14
Alim Salehzade <salehzade89@gmail.com>
2018-09-11 19:48:20 +0400

    .

--- SQL/index.sql
+++ SQL/index.sql
@@ -1,31 +1,35 @@
 
-DROP TABLE person
-CREATE TABLE person(
+--DROP TABLE person
+CREATE TABLE person
+(
     id INT IDENTITY(1,1) NOT NULL,
     name VARCHAR(200) NOT NULL,
     age int
 );
 GO
-DROP PROCEDURE insert_person
+--DROP PROCEDURE insert_person
 GO
 CREATE PROCEDURE dbo.insert_person
     @name  VARCHAR(200),
-    @age  int 
+    @age  int
 AS
-   INSERT into person VALUES(@name , @age);
+INSERT into person
+VALUES(@name , @age);
 GO
 EXECUTE dbo.insert_person 'person4',17;
 GO
 
-SELECT * FROM dbo.person
+SELECT *
+FROM dbo.person
 
 CREATE CLUSTERED INDEX index_age
 ON person
 (age)
 
-DROP TABLE rand_table
+--DROP TABLE rand_table
 
-CREATE TABLE rand_table(
+CREATE TABLE rand_table
+(
     id INT NOT NULL,
     rand_int INT NOT NULL,
     rand_string VARCHAR(255)
@@ -34,31 +38,34 @@ CREATE TABLE rand_table(
 SELECT ROUND(RAND()*100 , 0) AS VALUE
 
 
-DROP VIEW view_rand_int
+--DROP VIEW view_rand_int
 GO
 CREATE VIEW view_rand_int
 AS
-SELECT ROUND(RAND()*100 , 0) AS VALUE
+    SELECT ROUND(RAND()*100 , 0) AS VALUE
 GO
 
-SELECT * FROM view_rand_int
-DROP VIEW view_rand_string
+SELECT *
+FROM view_rand_int
+--DROP VIEW view_rand_string
 
 GO
 CREATE VIEW view_rand_string
 AS
-SELECT NEWID() as randString;
+    SELECT NEWID() as randString;
 GO
-DROP PROCEDURE insert_rand_table
+--DROP PROCEDURE insert_rand_table
 
-SELECT * FROM view_rand_string;
+SELECT *
+FROM view_rand_string;
 GO
 CREATE PROCEDURE insert_rand_table
-@id int,
-@randInt int,
-@randString VARCHAR(255)
-AS 
-INSERT INTO rand_table VALUES(@id , @randInt , @randString);
+    @id int,
+    @randInt int,
+    @randString VARCHAR(255)
+AS
+INSERT INTO rand_table
+VALUES(@id , @randInt , @randString);
 
 
 GO
@@ -68,18 +75,24 @@ DECLARE @randString VARCHAR(255)='';
 
 WHILE @count <10000
 BEGIN
-SELECT @randInt = [VALUE] FROM view_rand_int;
-SELECT @randString =randString FROM view_rand_string
-EXECUTE insert_rand_table @count , @randInt , @randString; 
-SET @count+=1;
+    SELECT @randInt = [VALUE]
+    FROM view_rand_int;
+    SELECT @randString =randString
+    FROM view_rand_string
+    EXECUTE insert_rand_table @count , @randInt , @randString;
+    SET @count+=1;
 END;
 
-SELECT * FROM rand_table WHERE rand_string = '004E52DF-EB76-4FE5-B645-FA8B058FE84A'
+SELECT *
+FROM rand_table
+WHERE rand_string = '004E52DF-EB76-4FE5-B645-FA8B058FE84A'
 
 SET STATISTICS TIME ON
 --CPU time = 16 ms, elapsed time = 16 ms. 
 --CPU time = 1217 ms, elapsed time = 1217 ms. 
-SELECT * FROM rand_table WHERE rand_int = 15
+SELECT *
+FROM rand_table
+WHERE rand_int = 15
 
 GO
 CREATE CLUSTERED INDEX index_rand_table_name
---
+++ SQL/transaction.sql
@@ -0,0 +1,86 @@
+CREATE DATABASE db1;
+USE db1;
+
+CREATE TABLE Student
+(
+    id INT identity(1,1),
+    name VARCHAR(40) NOT NULL,
+    surname VARCHAR(40) NOT NULL,
+    fin VARCHAR(10) NOT NULL unique,
+    contact VARCHAR(20) NOT NULL,
+    registration_date DATE NOT NULL,
+    status INT default 1
+);
+GO
+CREATE PROCEDURE insert_student
+    @name VARCHAR(40),
+    @surname VARCHAR(40),
+    @fin VARCHAR(10),
+    @contact VARCHAR(20),
+    @registration_date DATE,
+    @status int =1
+AS
+INSERT INTO Student
+VALUES(@name , @surname , @fin , @contact , @registration_date , @status)
+
+EXECUTE insert_student 'name1' , 'surname1' , 'fin1' , 'contact1' ,'11-09-2018';
+
+CREATE TABLE Teacher
+(
+    id INT identity(1,1),
+    name VARCHAR(40) NOT NULL,
+    surname VARCHAR(40) NOT NULL,
+    contact VARCHAR(20) NOT NULL,
+    status INT default 1
+);
+
+
+GO
+CREATE PROCEDURE insert_teacher
+    @name VARCHAR(40),
+    @surname VARCHAR(40),
+    @contact VARCHAR(20),
+    @status int =1
+AS
+--INSERT INTO Teacher VALUES(@name , @surname , @contact , @status);
+
+EXECUTE insert_teacher 'name1' , 'surname1' , 'contact1';
+
+CREATE TABLE Lesson
+(
+    id INT identity(1,1),
+    name VARCHAR(40) NOT NULL,
+    default_price INT NOT NULL,
+    status INT default 1
+);
+
+
+GO
+CREATE CLUSTERED INDEX index_studnet_name
+ON  Student (name)
+
+SELECT *
+FROM Student
+
+BEGIN TRANSACTION
+EXECUTE insert_student 'name3' , 'surname3' , 'fin3' , 'contact3' ,'11-09-2018';
+ROLLBACK TRANSACTION
+
+
+GO
+CREATE PROCEDURE student_insert_validate
+    @name VARCHAR(40),
+    @surname VARCHAR(40),
+    @fin VARCHAR(10),
+    @contact VARCHAR(20),
+    @registration_date DATE,
+    @status int =1
+AS
+BEGIN TRANSACTION
+EXECUTE insert_student @name , @surname , @fin , @contact ,@registration_date;
+IF LEN(@contact) = 13
+BEGIN
+    ROLLBACK TRANSACTION
+END;
+
+EXECUTE student_insert_validate 'name4' , 'surname4' , 'fin4' , '1234567891011' ,'11-09-2018';
