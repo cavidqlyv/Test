31732e7fbc6c19b2b1029ba8b213d11a2e134e45
Alim <salehzade89@gmail.com>
2018-09-13 03:01:35 +0400

    .

--- 11.17/t21.11.cpp
+++ 11.17/t21.11.cpp
@@ -12,16 +12,15 @@ using namespace std;
 #define KB_ESCAPE 27
 #define KB_ENTER 13
 
-void game(int * size, int difficulty)
-
+void game(int *size, int difficulty)
 {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	int move[2] = { 0,0 };
+	int move[2] = {0, 0};
 	bool flag = true;
 	int key;
 	int count = 0;
-	int ** scr = new int *[size[0]];
-	char ** scrw = new char *[size[0]];
+	int **scr = new int *[size[0]];
+	char **scrw = new char *[size[0]];
 	for (int i = 0; i < size[0]; i++)
 	{
 		scrw[i] = new char[size[1]];
@@ -37,24 +36,29 @@ void game(int * size, int difficulty)
 	}
 	int difficulty1;
 	int win = 0;
-	if (difficulty == 1) difficulty1 = size[0] * size[1] / 6;
-	else if (difficulty == 2) difficulty1 = size[0] * size[1] / 5;
-	else if (difficulty == 3) difficulty1 = size[0] * size[1] / 4;
-	{
-		int a, b;
-		int n;
-		time_t t;
+	if (difficulty == 1)
+		difficulty1 = size[0] * size[1] / 6;
+	else if (difficulty == 2)
+		difficulty1 = size[0] * size[1] / 5;
+	else if (difficulty == 3)
+		difficulty1 = size[0] * size[1] / 4;
 
-		n = 5;
-		srand((unsigned)time(&t));
-		for (int i = 0; i < difficulty1; i++)
-		{
-			a = rand() % size[0];
-			b = rand() % size[1];
-			if (scr[a][b] == 0) scr[a][b] = 1;
-			else i--;
-		}
+	int a, b;
+	int n;
+	time_t t;
+
+	n = 5;
+	srand((unsigned)time(&t));
+	for (int i = 0; i < difficulty1; i++)
+	{
+		a = rand() % size[0];
+		b = rand() % size[1];
+		if (scr[a][b] == 0)
+			scr[a][b] = 1;
+		else
+			i--;
 	}
+
 	int a = 0, b = 0;
 	char s = 254;
 	while (1)
@@ -73,7 +77,8 @@ void game(int * size, int difficulty)
 					SetConsoleTextAttribute(hConsole, 7);
 					cout << " ";
 				}
-				else cout << scrw[i][j] << " ";
+				else
+					cout << scrw[i][j] << " ";
 			}
 			cout << "\n";
 		}
@@ -99,9 +104,12 @@ void game(int * size, int difficulty)
 		{
 			move[0]++;
 		}
-		else if (key == KB_UP) move[0]--;
-		else if (key == KB_LEFT) move[1]--;
-		else if (key == KB_RIGHT) move[1]++;
+		else if (key == KB_UP)
+			move[0]--;
+		else if (key == KB_LEFT)
+			move[1]--;
+		else if (key == KB_RIGHT)
+			move[1]++;
 		else if (key == KB_ENTER)
 		{
 			int c = move[0];
@@ -122,17 +130,25 @@ void game(int * size, int difficulty)
 			{
 				if (c != 0)
 				{
-					if (scr[c - 1][e] == 1) count++;
-					if (scr[c - 1][e - 1] == 1) count++;
-					if (scr[c - 1][e + 1] == 1) count++;
+					if (scr[c - 1][e] == 1)
+						count++;
+					if (scr[c - 1][e - 1] == 1)
+						count++;
+					if (scr[c - 1][e + 1] == 1)
+						count++;
 				}
-				if (scr[c][e - 1] == 1 && e != 0) count++;
-				if (scr[c][e + 1] == 1 && e != size[1] - 1) count++;
+				if (scr[c][e - 1] == 1 && e != 0)
+					count++;
+				if (scr[c][e + 1] == 1 && e != size[1] - 1)
+					count++;
 				if (c != size[0] - 1)
 				{
-					if (scr[c + 1][e] == 1) count++;
-					if (scr[c + 1][e - 1] == 1) count++;
-					if (scr[c + 1][e + 1] == 1) count++;
+					if (scr[c + 1][e] == 1)
+						count++;
+					if (scr[c + 1][e - 1] == 1)
+						count++;
+					if (scr[c + 1][e + 1] == 1)
+						count++;
 				}
 				win++;
 				scrw[move[0]][move[1]] = count + 48;
@@ -145,7 +161,7 @@ void start()
 {
 	int key;
 	int keyarr[6];
-	int size[2] = { 5,5 };
+	int size[2] = {5, 5};
 	int difficulty = 2;
 	for (int i = 1; i < 6; i++)
 	{
@@ -171,9 +187,12 @@ void start()
 			SetConsoleTextAttribute(hConsole, 7);
 			cout << "\t\t\t\t" << size[0] << " x " << size[1] << "\n";
 			cout << "\n\t\t\tDifficulty\t\t\t";
-			if (difficulty == 1) cout << "Easy\n";
-			else if (difficulty == 2) cout << "Normal\n";
-			else cout << "Hard\n";
+			if (difficulty == 1)
+				cout << "Easy\n";
+			else if (difficulty == 2)
+				cout << "Normal\n";
+			else
+				cout << "Hard\n";
 			cout << "\n\n\n\t\t\t\t     ";
 			cout << "Start\n";
 			key = _getch();
@@ -187,7 +206,8 @@ void start()
 				keyarr[0] = 0;
 				keyarr[2] = 1;
 			}
-			else if (key == KB_ESCAPE) return;
+			else if (key == KB_ESCAPE)
+				return;
 		}
 		else if (keyarr[1] == 1) // difficulty
 		{
@@ -198,9 +218,12 @@ void start()
 			cout << "\n\t\t\tDifficulty\t\t\t";
 			SetConsoleTextAttribute(hConsole, 7);
 
-			if (difficulty == 1) cout << "Easy\n";
-			else if (difficulty == 2) cout << "Normal\n";
-			else cout << "Hard\n";
+			if (difficulty == 1)
+				cout << "Easy\n";
+			else if (difficulty == 2)
+				cout << "Normal\n";
+			else
+				cout << "Hard\n";
 			cout << "\n\n\n\t\t\t\t     ";
 			cout << "Start\n";
 
@@ -220,7 +243,8 @@ void start()
 				keyarr[1] = 0;
 				keyarr[5] = 1;
 			}
-			else if (key == KB_ESCAPE) return;
+			else if (key == KB_ESCAPE)
+				return;
 		}
 		else if (keyarr[2] == 1)
 		{
@@ -232,9 +256,12 @@ void start()
 			SetConsoleTextAttribute(hConsole, 7);
 			cout << " x " << size[1] << "\n";
 			cout << "\n\t\t\tDifficulty\t\t\t";
-			if (difficulty == 1) cout << "Easy\n";
-			else if (difficulty == 2) cout << "Normal\n";
-			else cout << "Hard\n";
+			if (difficulty == 1)
+				cout << "Easy\n";
+			else if (difficulty == 2)
+				cout << "Normal\n";
+			else
+				cout << "Hard\n";
 			cout << "\n\n\n\t\t\t\t     ";
 			cout << "Start\n";
 
@@ -263,19 +290,26 @@ void start()
 					cout << " x " << size[1] << "\n";
 					cout << "\n\t\t\tDifficulty\t\t\t";
 
-					if (difficulty == 1) cout << "Easy\n";
-					else if (difficulty == 2) cout << "Normal\n";
-					else cout << "Hard\n";
+					if (difficulty == 1)
+						cout << "Easy\n";
+					else if (difficulty == 2)
+						cout << "Normal\n";
+					else
+						cout << "Hard\n";
 					cout << "\n\n\n\t\t\t\t     ";
 					cout << "Start\n";
 
 					key = _getch();
-					if (key == KB_UP && size[0] < 20) size[0]++;
-					else if (key == KB_DOWN && size[0] > 5) size[0]--;
-					else if (key == KB_ENTER) break;
+					if (key == KB_UP && size[0] < 20)
+						size[0]++;
+					else if (key == KB_DOWN && size[0] > 5)
+						size[0]--;
+					else if (key == KB_ENTER)
+						break;
 				}
 			}
-			else if (key == KB_ESCAPE) return;
+			else if (key == KB_ESCAPE)
+				return;
 		}
 		else if (keyarr[3] == 1)
 		{
@@ -287,9 +321,12 @@ void start()
 			SetConsoleTextAttribute(hConsole, 7);
 			cout << "\n";
 			cout << "\n\t\t\tDifficulty\t\t\t";
-			if (difficulty == 1) cout << "Easy\n";
-			else if (difficulty == 2) cout << "Normal\n";
-			else cout << "Hard\n";
+			if (difficulty == 1)
+				cout << "Easy\n";
+			else if (difficulty == 2)
+				cout << "Normal\n";
+			else
+				cout << "Hard\n";
 			cout << "\n\n\n\t\t\t\t     ";
 			cout << "Start\n";
 
@@ -312,19 +349,26 @@ void start()
 					SetConsoleTextAttribute(hConsole, 7);
 					cout << "\n";
 					cout << "\n\t\t\tDifficulty\t\t\t";
-					if (difficulty == 1) cout << "Easy\n";
-					else if (difficulty == 2) cout << "Normal\n";
-					else cout << "Hard\n";
+					if (difficulty == 1)
+						cout << "Easy\n";
+					else if (difficulty == 2)
+						cout << "Normal\n";
+					else
+						cout << "Hard\n";
 					cout << "\n\n\n\t\t\t\t     ";
 					cout << "Start\n";
 
 					key = _getch();
-					if (key == KB_UP && size[1] < 20) size[1]++;
-					else if (key == KB_DOWN && size[1] > 5) size[1]--;
-					else if (key == KB_ENTER) break;
+					if (key == KB_UP && size[1] < 20)
+						size[1]++;
+					else if (key == KB_DOWN && size[1] > 5)
+						size[1]--;
+					else if (key == KB_ENTER)
+						break;
 				}
 			}
-			else if (key == KB_ESCAPE) return;
+			else if (key == KB_ESCAPE)
+				return;
 		}
 		else if (keyarr[4] == 1)
 		{
@@ -333,9 +377,12 @@ void start()
 			cout << "\t\t\t\t" << size[0] << " x " << size[1] << "\n";
 			cout << "\n\t\t\tDifficulty\t\t\t";
 			SetConsoleTextAttribute(hConsole, 112);
-			if (difficulty == 1) cout << "Easy\n";
-			else if (difficulty == 2) cout << "Normal\n";
-			else cout << "Hard\n";
+			if (difficulty == 1)
+				cout << "Easy\n";
+			else if (difficulty == 2)
+				cout << "Normal\n";
+			else
+				cout << "Hard\n";
 			SetConsoleTextAttribute(hConsole, 7);
 			cout << "\n\n\n\t\t\t\t     ";
 			cout << "Start\n";
@@ -356,21 +403,28 @@ void start()
 					cout << "\t\t\t\t" << size[0] << " x " << size[1] << "\n";
 					cout << "\n\t\t\tDifficulty\t\t\t";
 					SetConsoleTextAttribute(hConsole, 112);
-					if (difficulty == 1) cout << "Easy\n";
-					else if (difficulty == 2) cout << "Normal\n";
-					else cout << "Hard\n";
+					if (difficulty == 1)
+						cout << "Easy\n";
+					else if (difficulty == 2)
+						cout << "Normal\n";
+					else
+						cout << "Hard\n";
 					SetConsoleTextAttribute(hConsole, 7);
 					cout << "\n\n\n\t\t\t\t     ";
 					cout << "Start\n";
 
 					key = _getch();
-					if (key == KB_UP && difficulty > 1)difficulty--;
-					else if (key == KB_DOWN && difficulty < 3)difficulty++;
-					else if (key == KB_ENTER) break;
+					if (key == KB_UP && difficulty > 1)
+						difficulty--;
+					else if (key == KB_DOWN && difficulty < 3)
+						difficulty++;
+					else if (key == KB_ENTER)
+						break;
 				}
 			}
 
-			else if (key == KB_ESCAPE) return;
+			else if (key == KB_ESCAPE)
+				return;
 		}
 		else if (keyarr[5] == 1)
 		{
@@ -381,9 +435,12 @@ void start()
 			cout << " x " << size[1] << "\n";
 			cout << "\n\t\t\tDifficulty\t\t\t";
 
-			if (difficulty == 1) cout << "Easy\n";
-			else if (difficulty == 2) cout << "Normal\n";
-			else cout << "Hard\n";
+			if (difficulty == 1)
+				cout << "Easy\n";
+			else if (difficulty == 2)
+				cout << "Normal\n";
+			else
+				cout << "Hard\n";
 
 			cout << "\n\n\n\t\t\t\t     ";
 			SetConsoleTextAttribute(hConsole, 112);
@@ -392,8 +449,10 @@ void start()
 			cout << '\n';
 			key = _getch();
 
-			if (key == KB_ENTER) game(size, difficulty);
-			else if (key == KB_ESCAPE) return;
+			if (key == KB_ENTER)
+				game(size, difficulty);
+			else if (key == KB_ESCAPE)
+				return;
 			else if (key == KB_UP)
 			{
 				keyarr[5] = 0;
@@ -403,12 +462,12 @@ void start()
 	}
 }
 
-void main()
+int main()
 {
 	int key;
-	unsigned char frame[] = { 201,205,205,205,205,205,205,205,205,205,205 , 187 , '\0' };
-	unsigned char frame1[] = { 186,'\0' };
-	unsigned char frame2[] = { 200,205,205,205,205,205,205,205,205,205,205 , 188 , '\0' };
+	unsigned char frame[] = {201, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 187, '\0'};
+	unsigned char frame1[] = {186, '\0'};
+	unsigned char frame2[] = {200, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 188, '\0'};
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	bool menu1 = true;
 	while (1)
@@ -441,16 +500,22 @@ void main()
 		switch (key)
 		{
 		case KB_UP:
-			if (menu1 == true) menu1 = false;
-			else menu1 = true;
+			if (menu1 == true)
+				menu1 = false;
+			else
+				menu1 = true;
 			break;
 		case KB_DOWN:
-			if (menu1 == true) menu1 = false;
-			else menu1 = true;
+			if (menu1 == true)
+				menu1 = false;
+			else
+				menu1 = true;
 			break;
 		case KB_ENTER:
-			if (menu1 == false) return;
-			else start();
+			if (menu1 == false)
+				return 0;
+			else
+				start();
 			break;
 
 		default:
