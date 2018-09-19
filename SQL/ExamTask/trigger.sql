1be1646d56ef7d77ee7688c655cc6760d1147455
Alim Salehzade <salehzade89@gmail.com>
2018-09-19 19:16:39 +0400

    .

--- SQL/ExamTask/trigger.sql
+++ SQL/ExamTask/trigger.sql
@@ -1,1 +1,17 @@
 use examtask
+GO
+CREATE TRIGGER delete_group
+ON group_student
+after UPDATE
+AS
+BEGIN
+
+END
+GO
+
+select distinct g.name , g.id from [group] g
+ join  group_student gs on g.id = gs.id 
+
+select * from group_student where id = 1
+
+SELECT * FROM [group]
