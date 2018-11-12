641268859f4f23193a583853fbda365899ac8b40
Alim Salehzade <salehzade89@gmail.com>
2018-11-12 21:33:04 +0400

    .

---
+++ Js/Js1/main.js
@@ -0,0 +1,71 @@
+// console.log("Hello World!");
+// console.log(123);
+// console.log("123");
+// alert("aaa");
+
+// var undef = undefined;
+// var nll = null;
+
+// var num = 123;
+// var str = "string";
+// var arr = [1, "String", null, undefined];
+// var func = function () {
+
+// }
+
+// function func2() {
+
+// }
+
+// var bool = true;
+
+// var empty;
+
+// console.log(arr);
+
+
+// var num2 = new Number(3);
+
+// var str2 = new String("Salam");
+
+// console.log(num2, str2);
+
+// var x = 5;
+// var y = 10;
+
+// console.log(x % y);
+
+// var name = "aaa";
+
+// var copyname = name;
+
+// name= null;
+
+// console.log(name);
+// console.log(copyname);
+
+// var a=1;
+// var b= a;
+
+// a=null;
+
+// console.log(a);
+// console.log(b);
+
+var email = prompt("Enter email", "example@example.com");
+
+console.log(email);
+
+var x = 5;
+var y = 4;
+if (y > x) {
+    console.log("Bigger");
+
+} else if (x == y) {
+    console.log("Equal");
+
+} else {
+    console.log("Smaller");
+}
+
+
