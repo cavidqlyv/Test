1b4e6ace93de9c25dc346871700a08cfe491ed94
Alim <salehzade89@gmail.com>
2018-12-15 14:07:43 +0400

    .

--- Exam/HTML_CSS_JS/js/script.js
+++ Exam/HTML_CSS_JS/js/script.js
@@ -1,23 +1,49 @@
-//#region Search input
-if ($(window).width() >= 768) {
-    $('.navbar ul').superfish();
+var searchInput = document.getElementById("search-input");
+console.log(searchInput);
+searchInput.onfocus = function () {
+    this.style.width = "150px";
+    if (this.value == "Search")
+        this.value = "";
 }
-
-
-(function () {
-    var searchInput = document.getElementById("search-input");
-    console.log(searchInput);
-    searchInput.onfocus = function () {
-        this.style.width = "150px";
-        if (this.value == "Search")
-            this.value = "";
+searchInput.onblur = function () {
+    this.style.width = "42px";
+    if (this.value == "")
+        this.value = "Search";
+}
+jQuery(document).ready(function () {
+    if ($(window).width() >= 768) {
+        $('.navbar ul').superfish();
     }
-    searchInput.onblur = function () {
-        this.style.width = "42px";
-        if (this.value == "")
-            this.value = "Search";
-    }
-}())
 
+    var width = $(window).width();
+    $(window).on('resize', function () {
+        if ($(this).width() != width) {
+            width = $(this).width();
+            console.log(width);
+        }
+    });
 
-//#endregion
+    function changeWiev() {
+        "use strict";
+
+        if ($(window).width() < 768) {
+            if (jQuery("#menu-mobile").length == 0) {
+
+                jQuery('.menu .container').prepend('<a id="menu-mobile" href="javascript:void(0)" class="menu_toggler"><span class="fa fa-align-justify"></span></a>');
+                jQuery('.navbar').hide();
+                jQuery('.search').hide();
+
+                jQuery('.menu_toggler, .navbar ul li a').click(function () {
+                    jQuery('header .navbar').slideToggle(300);
+                });
+            }
+        }
+        else {
+            jQuery("#menu-mobile").remove();
+            jQuery('header .navbar').show();
+            jQuery('.search').show();
+        }
+    };
+    changeWiev();
+    $(window).resize(changeWiev);
+});
