1ff08c0c8dfd7c95e7e12492fe7d1ebed1f06048
alim salehzade <salehzade89@gmail.com>
2018-12-17 21:52:32 +0400

    .

--- Exam/HTML_CSS_JS/js/script.js
+++ Exam/HTML_CSS_JS/js/script.js
@@ -39,54 +39,119 @@ jQuery(document).ready(function () {
     // $(window).resize(changeWiev);
 });
 
-jQuery(window).load(function () {
-    //Top Slider
-    $('.flexslider.top_slider').flexslider({
-        animation: "fade",
-        controlNav: false,
-        directionNav: true,
-        animationLoop: false,
-        slideshow: false,
-        prevText: "",
-        nextText: "",
-        sync: "#carousel"
-    });
-    $('#carousel').flexslider({
-        animation: "fade",
-        controlNav: false,
-        animationLoop: false,
-        directionNav: false,
-        slideshow: false,
-        itemWidth: 100,
-        itemMargin: 5,
-        asNavFor: '.top_slider'
-    });
-
-    homeHeight();
-
-
-    // jQuery('.flexslider.top_slider .flex-direction-nav').addClass('container');
-
-
-    // //Vision Slider
-    // $('.flexslider.portfolio_single_slider').flexslider({
-    //     animation: "fade",
-    //     controlNav: true,
-    //     directionNav: true,
-    //     animationLoop: false,
-    //     slideshow: false,
-    // });
-
-
+jQuery(window).load(function(){
+	//Top Slider
+	$('.flexslider.top_slider').flexslider({
+		animation: "fade",
+		controlNav: false,
+		directionNav: true,
+		animationLoop: false,
+		slideshow: false,
+		prevText: "",
+		nextText: "",
+		sync: "#carousel"
+	});
+	$('#carousel').flexslider({
+		animation: "fade",
+		controlNav: false,
+		animationLoop: false,
+		directionNav: false,
+		slideshow: false,
+		itemWidth: 100,
+		itemMargin: 5,
+		asNavFor: '.top_slider'
+	});
+	
+	homeHeight();
+	
+	
+	jQuery('.flexslider.top_slider .flex-direction-nav').addClass('container');
+	
+	
+	//Vision Slider
+	$('.flexslider.portfolio_single_slider').flexslider({
+		animation: "fade",
+		controlNav: true,
+		directionNav: true,
+		animationLoop: false,
+		slideshow: false,
+	});
+	
+	
 });
 
-jQuery(window).resize(function () {
-    homeHeight();
-
-    changeWiev();
+jQuery(window).resize(function(){
+	homeHeight();
+	
+	white_mobile_menu();
+	
 });
 
-function homeHeight() {
-    var wh = jQuery(window).height() - 80;
-    jQuery('.top_slider, .top_slider .slides li').css('height', wh);
+jQuery(document).ready(function(){
+	homeHeight();
+	
+});
+
+function homeHeight(){
+	var wh = jQuery(window).height() - 80;
+	jQuery('.top_slider, .top_slider .slides li').css('height', wh);
 }
+
+
+
+
+
+
+
+
+
+/*-----------------------------------------------------------------------------------*/
+/*	OWLCAROUSEL
+/*-----------------------------------------------------------------------------------*/
+$(document).ready(function() {
+	
+	//WORKS SLIDER
+    var owl = $(".owl-demo.projects_slider");
+
+    owl.owlCarousel({
+		navigation: true,
+		pagination: false,
+		items : 4,
+		itemsDesktop : [1000,4],
+		itemsDesktop : [600,3]
+	});
+	
+	
+	//TEAM SLIDER
+    var owl = $(".owl-demo.team_slider");
+
+    owl.owlCarousel({
+		navigation: true,
+		pagination: false,
+		items : 3,
+		itemsDesktop : [600,2]
+	});
+	
+	
+	
+	jQuery('.owl-controls').addClass('container');
+	
+	
+	//TESTIMONIALS SLIDER
+    var owl = $(".owl-demo.testim_slider");
+
+    owl.owlCarousel({
+		itemsCustom : [
+			[0, 1]
+        ],
+		navigation: false,
+		pagination: true,
+		items : 1
+	});
+	
+	
+	
+	jQuery('.owl-controls').addClass('container');
+	
+	
+});
